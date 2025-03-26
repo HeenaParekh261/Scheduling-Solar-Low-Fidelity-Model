9da9671c65f4697f6296c235166a8cf6c9e6311a
HeenaParekh261 <heenaparekh261@gmail.com>
2025-03-26 15:49:54 -0400

    Create Population.py

---
+++ Population.py
@@ -0,0 +1,22 @@
+# Base code from https://gist.github.com/AnasBrital98/6cc9964230a3aa956d78c6c22ab98616#file-populationexample1-py
+
+class Population :
+    
+    def __init__(self , populationSize , chromosomeSize , function , init):
+        self.chromosomes = []
+        if init :
+            self.chromosomes = [Chromosome(chromosomeSize , function) for i in range(populationSize)]
+            self.chromosomes.sort(key = lambda x:x.fitness)
+            self.fittest = self.chromosomes[0]
+    
+    def getNFittestChromosomes(self, n):
+        self.chromosomes.sort(key = lambda x:x.fitness)
+        return self.chromosomes[:n]
+    
+    def findTheFittest(self):
+           self.chromosomes.sort(key = lambda x:x.fitness)
+           self.fittest = self.chromosomes[0]
+    
+    def calculateTheFitnessForAll(self):
+        for chromosome in self.chromosomes:
+            chromosome.calculateTheFitness()
