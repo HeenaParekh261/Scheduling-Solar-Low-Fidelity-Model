04d9d9ea224ffcc4583e734ebd50d1fe749cb79a
coolBill223 <bzhou6198@gmail.com>
2025-04-02 15:06:26 -0400

    update chromosome

--- chromosome.py
+++ chromosome.py
@@ -1,4 +1,5 @@
 #Base code from https://gist.github.com/AnasBrital98/0fe7043beeda9d116ec1668350c33a22#file-chromosomeexample2-py
+import numpy as np
 class Chromosome:
     def __init__(self , NeuralNetwork , weights = None , biases = None):
         self.NeuralNetwork = NeuralNetwork
@@ -7,3 +8,10 @@ class Chromosome:
         self.fitness = self.calculateFitness()
         
     def calculateFitness(self):
+        decimalValueOfGenes = self.convertToDecimal()
+        fitnessValue = self.function(decimalValueOfGenes)
+        self.fitness = fitnessValue
+        
+    def convertToDecimal(self):
+        decimal = sum([pow(2 , i) * int(x) for x,i in zip(self.genes , reversed(range(len(self.genes))))])
+        return decimal
